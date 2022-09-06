1af6cd9a5acd36749b064e60ec51b8528cccacda
arth2002 <kingap12ka4@gmail.com>
2022-09-06 16:33:06 +0530

    some github cards and list component added

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -1,4 +1,4 @@
-import React, { useEffect,useState } from 'react';
+import React  from 'react';
 import Iframely from './Iframely';
 import './Blogs.css';
 
---
+++ src/components/List.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const List = () => {
+  return (
+    
+    <>
+        <div className="items_main">
+            <hr />
+            <div className="items">
+                <span>👉 200+ Leetcode Questions Solved</span>
+            </div>
+            <hr />
+            <div className="items">
+                <span>👉 5⭐️ on HackerRank(Python)</span>
+            </div>
+            <hr />
+            <div className="items">
+                <span>👉 5+ Open-Source Contributions(Repo like, LinkFree,Open-sauced)</span>
+            </div>
+            <hr />
+            <div className="items">
+                <span>👉 5⭐️ on HackerRank(30Days of Code)</span>
+            </div>
+            <hr />
+        </div>
+
+    </>
+  )
+}
+
+export default List;
