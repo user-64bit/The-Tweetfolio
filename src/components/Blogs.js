4ef632a6459a3a4af2aa7695d14fe17e9521d1dd
Mister-H <hs913271@gmail.com>
2022-10-01 14:24:56 +0530

    Posts card changed used jsonlink.io to extract meta information created BlogCard.js component

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -1,6 +1,6 @@
 import React  from 'react';
-import Iframely from './Iframely';
 import './Blogs.css';
+import BlogCard from './BlogCard';
 
 
 const Blogs = () => {
@@ -13,23 +13,31 @@ const Blogs = () => {
             </h2>
           </div>
            <div className="posts">
-              <Iframely url='https://iamprogrammer.hashnode.dev/digital-root' size='small'/>
-              <Iframely url='https://iamprogrammer.hashnode.dev/twitter-bot-using-python-and-deployed-on-heroku' size='small'/>
-              <Iframely url='https://iamprogrammer.hashnode.dev/sliding-window-technique' size='small'/>
-              <Iframely url='https://iamprogrammer.hashnode.dev/pascals-triangle' size='small'/>
-              <Iframely url='https://apnacodingadda.blogspot.com/2022/04/what-is-docker.html' size='small'/>
-              <Iframely url='https://apnacodingadda.blogspot.com/2022/03/makefile.html' size='small'/>
-              <Iframely url='https://apnacodingadda.blogspot.com/2022/02/what-is-digital-root.html' size='small'/>
-              <Iframely url='https://apnacodingadda.blogspot.com/2022/02/coding-problems-google-kick-start-2021.html' size='small'/>
-              <Iframely url='https://apnacodingadda.blogspot.com/2022/02/instagram-osint.html' size='small'/>
-              <Iframely url='https://apnacodingadda.blogspot.com/2022/02/how-to-connect-mysql-using-c-in-linux.html' size='small'/>
-              <Iframely url='https://apnacodingadda.blogspot.com/2022/01/args-and-kwargs-in-python.html' size='small'/>
-              <Iframely url='https://apnacodingadda.blogspot.com/2022/01/find-nth-fibonacci-number-time-o1.html' size='small'/>
-              <Iframely url='https://apnacodingadda.blogspot.com/2022/01/how-to-create-website-on-dark-web.html' size='small'/>
-              <Iframely url='https://apnacodingadda.blogspot.com/2022/01/what-are-nfts-how-to-earn-create-and.html' size='small'/>
-              <Iframely url='https://apnacodingadda.blogspot.com/2021/11/extension-create-publish-and-earn.html' size='small'/>
-              <Iframely url='https://apnacodingadda.blogspot.com/2021/11/coding-problems-hacker-rank-separate.html' size='small'/>
-              <Iframely url='https://apnacodingadda.blogspot.com/2021/11/binary-search.html' size='small'/>
+              {/*<Iframely url='https://iamprogrammer.hashnode.dev/digital-root' size='small'/>*/}
+              {/*<Iframely url='https://iamprogrammer.hashnode.dev/twitter-bot-using-python-and-deployed-on-heroku' size='small'/>*/}
+              {/*<Iframely url='https://iamprogrammer.hashnode.dev/sliding-window-technique' size='small'/>*/}
+              {/*<Iframely url='https://iamprogrammer.hashnode.dev/pascals-triangle' size='small'/>*/}
+              {/*<Iframely url='https://apnacodingadda.blogspot.com/2022/04/what-is-docker.html' size='small'/>*/}
+              {/*<Iframely url='https://apnacodingadda.blogspot.com/2022/03/makefile.html' size='small'/>*/}
+              {/*<Iframely url='https://apnacodingadda.blogspot.com/2022/02/what-is-digital-root.html' size='small'/>*/}
+              {/*<Iframely url='https://apnacodingadda.blogspot.com/2022/02/coding-problems-google-kick-start-2021.html' size='small'/>*/}
+              {/*<Iframely url='https://apnacodingadda.blogspot.com/2022/02/instagram-osint.html' size='small'/>*/}
+              {/*<Iframely url='https://apnacodingadda.blogspot.com/2022/02/how-to-connect-mysql-using-c-in-linux.html' size='small'/>*/}
+              {/*<Iframely url='https://apnacodingadda.blogspot.com/2022/01/args-and-kwargs-in-python.html' size='small'/>*/}
+              {/*<Iframely url='https://apnacodingadda.blogspot.com/2022/01/find-nth-fibonacci-number-time-o1.html' size='small'/>*/}
+              {/*<Iframely url='https://apnacodingadda.blogspot.com/2022/01/how-to-create-website-on-dark-web.html' size='small'/>*/}
+              {/*<Iframely url='https://apnacodingadda.blogspot.com/2022/01/what-are-nfts-how-to-earn-create-and.html' size='small'/>*/}
+              {/*<Iframely url='https://apnacodingadda.blogspot.com/2021/11/extension-create-publish-and-earn.html' size='small'/>*/}
+              {/*<Iframely url='https://apnacodingadda.blogspot.com/2021/11/coding-problems-hacker-rank-separate.html' size='small'/>*/}
+              {/*<Iframely url='https://apnacodingadda.blogspot.com/2021/11/binary-search.html' size='small'/>*/}
+            <BlogCard url='https://iamprogrammer.hashnode.dev/digital-root'/>
+            <BlogCard url='https://iamprogrammer.hashnode.dev/twitter-bot-using-python-and-deployed-on-heroku'/>
+            <BlogCard url='https://iamprogrammer.hashnode.dev/sliding-window-technique'/>
+            <BlogCard url='https://iamprogrammer.hashnode.dev/pascals-triangle'/>
+            <BlogCard url='https://apnacodingadda.blogspot.com/2022/04/what-is-docker.html'/>
+            {/*<BlogCard url='https://apnacodingadda.blogspot.com/2022/03/makefile.html'/>*/}
+            {/*<BlogCard url='https://apnacodingadda.blogspot.com/2022/02/what-is-digital-root.html'/>*/}
+            <BlogCard url='https://apnacodingadda.blogspot.com/2022/02/coding-problems-google-kick-start-2021.html'/>
            </div>
         </div>
 
