b73c02c23e7d552d1edb8118f50d7b72f5a02cd6
0xuser64bit <76396335+0xuser64bit@users.noreply.github.com>
2022-10-21 16:59:04 +0530

    started making responsive

---
+++ src/Responsive/Main.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ResponsiveNavbar from './ResponsiveNavbar'
+
+const Main = () => {
+  return (
+    <>
+      <ResponsiveNavbar/>
+    
+     </>
+  )
+}
+
+export default Main
