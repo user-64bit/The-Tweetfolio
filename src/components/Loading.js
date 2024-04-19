54cf1eb29b28ead3a9fd9eb2843dcf80f956c136
0xuser64bit <76396335+0xuser64bit@users.noreply.github.com>
2024-04-19 19:57:35 +0530

    reset

--- src/components/Loading.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactLoading from "react-loading";
-
-const Loading = ({ type, color }) => (
-  <ReactLoading type={type} color={color} height={100} width={100} />
-);
-
-export default Loading;
