011e13a478d6614c78288615f46dff3096e3c07f
0xuser64bit <76396335+0xuser64bit@users.noreply.github.com>
2026-04-06 11:22:11 +0530

    refactor: update contact email, remove resume link, and restyle sidebar connection icons

--- src/components/sidebar/RightSidebar.tsx
+++ src/components/sidebar/RightSidebar.tsx
@@ -1,30 +1,38 @@
 import React from "react";
 import { FaGithub } from "react-icons/fa";
-import { HiOutlineMail } from "react-icons/hi";
-import PROFILE_IMAGE from "../../assets/profile.jpg";
+import { SiGmail } from "react-icons/si";
 import { GITHUB_USERNAME, X_USERNAME, CONTACT_EMAIL } from "../../config";
+
+const XIcon = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" width="20" height="20" viewBox="0 0 24 24">
+    <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z" />
+  </svg>
+);
+
 
 const connectLinks = [
   {
     name: "GitHub",
     handle: `@${GITHUB_USERNAME}`,
-    icon: <FaGithub className="text-xl" />,
+    icon: <FaGithub className="text-[22px]" />,
+    iconBg: "bg-[#24292e]",
+    iconColor: "text-white",
     url: `https://github.com/${GITHUB_USERNAME}`,
   },
   {
     name: "X / Twitter",
     handle: `@${X_USERNAME}`,
-    icon: (
-      <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" width="20" height="20" viewBox="0 0 50 50">
-        <path d="M 11 4 C 7.134 4 4 7.134 4 11 L 4 39 C 4 42.866 7.134 46 11 46 L 39 46 C 42.866 46 46 42.866 46 39 L 46 11 C 46 7.134 42.866 4 39 4 L 11 4 z M 13.085938 13 L 21.023438 13 L 26.660156 21.009766 L 33.5 13 L 36 13 L 27.789062 22.613281 L 37.914062 37 L 29.978516 37 L 23.4375 27.707031 L 15.5 37 L 13 37 L 22.308594 26.103516 L 13.085938 13 z M 16.914062 15 L 31.021484 35 L 34.085938 35 L 19.978516 15 L 16.914062 15 z" />
-      </svg>
-    ),
+    icon: <XIcon />,
+    iconBg: "bg-black",
+    iconColor: "text-white",
     url: `https://x.com/${X_USERNAME}`,
   },
   {
     name: "Email",
     handle: CONTACT_EMAIL,
-    icon: <HiOutlineMail className="text-xl" />,
+    icon: <SiGmail className="text-[20px]" />,
+    iconBg: "bg-white",
+    iconColor: "text-[#EA4335]",
     url: `mailto:${CONTACT_EMAIL}`,
   },
 ];
@@ -43,8 +51,8 @@ const RightSidebar = () => {
             rel="noreferrer"
             className="flex items-center gap-3 px-4 py-3 hover:bg-x-tertiary transition-colors"
           >
-            <div className="w-10 h-10 rounded-full overflow-hidden flex-shrink-0">
-              <img src={PROFILE_IMAGE} alt={link.name} className="w-full h-full object-cover" />
+            <div className={`w-10 h-10 rounded-full flex-shrink-0 flex items-center justify-center ${link.iconBg} ${link.iconColor}`}>
+              {link.icon}
             </div>
             <div className="flex-1 min-w-0">
               <p className="text-[15px] font-bold text-x-text-primary truncate">{link.name}</p>
